7849c816e830ee53ce4aac4ddcc72825d5504936
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2023-04-30 04:23:40 +0000

    Improve startup automation.

--- .config/devbox/init-db.sh
+++ .config/devbox/init-db.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+start_db() {
+    devbox services up --background
+    echo "Waiting for Postgres to start..."
+    timeout 20 bash -c 'until printf \"\" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 3; done' localhost 5432
+}
+
 if [ ! -f "$PGDATA/PG_VERSION" ]; then
     initdb -D "$PGDATA" -U postgres
-    psql -U postgres -f ./init-db.sql
+    start_db
+    psql -U postgres -f ./.config/devbox/init-db.sql
+else
+    start_db
 fi
